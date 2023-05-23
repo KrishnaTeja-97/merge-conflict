7c8273f2abe6f148de517e9a3dcaacad90fc05f3
root <root@ip-172-31-90-235.ec2.internal>
2023-05-23 23:29:06 +0000

    changes in main.c

--- main.c
+++ main.c
@@ -2,12 +2,12 @@ pseudo code
 {
 Initial Function()
 {
-  //Sample Code
+  Here changes occured in main branch 
 }
 
 Initial Security()
 {
- //Sample Code
+ here cahnges occured in main branch
 }
 
 
